fb56507a1a42b4a41fcdd35878cc4a25e6cbd02f
Thomas Sileghem <tsileghem@neworbit.co.uk>
2016-01-22 12:52:45 +0000

    feat(object): implements object validation

---
+++ test/object.js
@@ -0,0 +1,59 @@
+import test from 'ava';
+
+import schemaValidator from '../lib/index';
+
+test('validate an object', t => {
+    t.ok(schemaValidator({
+        anObject: {
+            aString: 'string',
+            aNumber: 'number'
+        }
+    })({
+        anObject: {
+            aString: 'test',
+            aNumber: 2
+        }
+    }));
+});
+
+test('don\'t validate an object', t => {
+    t.throws(() => schemaValidator({
+        anObject: {
+            aString: 'string',
+            aNumber: 'number'
+        }
+    })({
+        anObject: {
+            aString: 2,
+            aNumber: 2
+        }
+    }), /Expected 'aString' to be a string/);
+});
+
+test('validate a nested object', t => {
+    t.ok(schemaValidator({
+        object1: {
+            object2: {
+                object3: {
+                    object4: {
+                        object5: {
+                            aString: 'string'
+                        }
+                    }
+                }
+            }
+        }
+    })({
+        object1: {
+            object2: {
+                object3: {
+                    object4: {
+                        object5: {
+                            aString: 'test'
+                        }
+                    }
+                }
+            }
+        }
+    }));
+});
